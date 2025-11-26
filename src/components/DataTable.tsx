e9762c1f90751ce549240a05db49551cd6ca4846
Farkas Bence <165921343+benyo22@users.noreply.github.com>
2025-11-26 11:34:50 +0100

    remove custom sorting condition from DataTable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -27,7 +27,7 @@ interface DataTableProps<TData, TValue> {
 }
 
 export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = React.useState<SortingState>([{ id: "start", desc: true }]);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
 
   const table = useReactTable({
     data,
