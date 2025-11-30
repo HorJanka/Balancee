ddb808d7fd9caf7b9ae1347cfdc73d03a0066974
Farkas Bence <165921343+benyo22@users.noreply.github.com>
2025-11-30 01:18:03 +0100

    correct imports, on save prettier format

--- src/app/categories/CategoryTable.tsx
+++ src/app/categories/CategoryTable.tsx
@@ -1,12 +1,11 @@
-"use client"
-import { DataTable } from "../../components/DataTable"
+"use client";
+import { DataTable } from "@/components/DataTable";
 import { CategoryColumn, columns } from "./columns";
 
-export default function CategoryTable({categories} : {categories: CategoryColumn[]}) {
-    
-    return (
-        <div>
-            <DataTable columns={columns} data={categories} pageSize={10} />
-        </div>
-    )
+export default function CategoryTable({ categories }: { categories: CategoryColumn[] }) {
+  return (
+    <div>
+      <DataTable columns={columns} data={categories} pageSize={10} />
+    </div>
+  );
 }
